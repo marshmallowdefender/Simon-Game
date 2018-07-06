60afed2ce9b25f25d7bd6e1364232119ab012429
Ashley Thompson <ashley.thompson769@gmail.com>
2018-07-06 15:22:37 -0400

    Created initial variables and random color function

--- js/main.js
+++ js/main.js
@@ -38,101 +38,33 @@ where they can pick their number of rounds
 
 //I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.(done) 
 
-
-
-// get elements from html and style their colors
-const redEl = document.querySelector('#red').style.background = 'red'; 
-const blueEl = document.querySelector('#blue').style.background = "blue";
-const yellowEl = document.querySelector('#yellow').style.background = "yellow";
-const greenEl = document.querySelector('#green').style.background = "green";
-
-
-/*
- Game properties
-let game = {
-    count = 0,
-    colors = ['red','blue','yellow','green'],
-    currentGame = [], //array tracking the order of colors to memorize
-    player: [],
+const buttons = {
+    red: document.querySelector('#red'),
+    blue: document.querySelector('#blue'),
+    yellow: document.querySelector('#yellow'),
+    green: document.querySelector('#green')
 }
-*/
+const targetPattern  = [];
+const colors = ["red","blue","yellow","green"];
 
-//rand integers pulled from an array and entered into another array
-//math.random value 0-1
-//multiply by 4
-//floor division
-
-//Create a function that picks a random color from an array of colors
-//Math.floor returns the largest integer less than or equal to a given number
-//math.random returns a floating-point, pseudo-random number in the range of 0-1( inclusive of 0, but not 1)
-
-
-let shuffleColors = [redEl, blueEl, yellowEl, greenEl];
-let colorArray = [];
-for (let i = 0; i < 4 ; i++) {
-    let pickColor = shuffleColors[(Math.floor(Math.random()* 4))];
-    //let pickColor = shuffleColors[(Math.floor(Math.random()* shuffleColors.length))];
-    if (colorArray[pickColor]){
-        colorArray[pickColor] ++
-    } else {
-        colorArray[pickColor] = 1
-    }
- }
-
-
-
-console.log(colorArray); //Picks radomly from the array and return 4 random choices from the array
-
-
-
-//whichever color is picked, then it lights up a brighter shade
-function lightUp() {
-let redLight = redEl;
-let blueLight = blueEl;
-let yellowLight = yellowEl;
-let greenLight = greenEl;
-
-if (colorArray === redEl) {
-    redLight.style.backgroundColor = "rgb(110, 244, 66"
-} else if (colorArray === blueEl) {
-    blueLight.style.backgroundColor = "rgb(66, 173, 244)"
-} else if (colorArray === yellowEl) {
-    yellowLight.style.backgroundColor = "rgb(244, 215, 66)"
-} else if (colorArray === greenEl) {
-    greenLight.style.backgroundColor = "rgb(66, 244, 83)"
-    } 
-    return lightUp;
+function randomColor() {
+    return colors[Math.floor(Math.random()*colors.length)]
 }
+randomColor();
 
-
-//Create a function that gives out random color sequences starting at 1 color, then increments after
-// player successfully finishes their turn
-
-
-//create a function to handle button click events
-
-let gameButtons = document.querySelector('.simon-button');
-for (i = 0; i < gameButtons.length; i++) {
-    buttons[i].addEventListener('click' , handleClick)
-}
-
+//Use targetPattern[i] to see current color
+//Use buttons[currentColor] to get target button
+//toggle flash class on target button
+//If i === targetPattern.length clear interval
 
 
 
 /*
 
-function clearGame() { //setting currentGame to a point to an empty array that there
-    //is no sequence yet. Essentially sets an empty board.
-    game.currentGame = [];
-    game.count = 0;
-    addCount();
-  }
-  
-  function newGame() {
-    clearGame();
-  }
+function playPattern(){
+    let i = 0;
+    const id = setInterval(function(){}
 
+}
+}
 */
-
-
-
